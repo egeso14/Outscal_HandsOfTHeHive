0061cb68cc7079edfa8df45d8d234f441cb68107
egeso14 <egeyilmaz2023@u.northwestern.edu>
2025-05-19 20:04:32 -0500

    fixinf the collision avoidance behavior

--- Assets/Scripts/MovementBehaviors/CollisionAvoidanceBehavior.cs
+++ Assets/Scripts/MovementBehaviors/CollisionAvoidanceBehavior.cs
@@ -19,13 +19,17 @@ public class CollisionAvoidanceBehavior : ISteeringBehavior
         Collider bestTarget = null;
         float bestMinDistance = 0;
         float bestCurrentDistance = 0;
-        float shortestTimeToCollision = 0;
+        Vector3 bestCurrentRelativePosition = Vector3.zero;
+        Vector3 bestRelativePosition = Vector3.zero;
+        Vector3 bestRelativeVelocity = Vector3.zero;
+        float bestTimeOfClosestApproach = 0;
+
         Collider[] collidersInRange = Physics.OverlapSphere(rootTransform.position, detectionRadius, detectionMask);
         foreach (Collider collider in collidersInRange)
         {
             Rigidbody otherBody = collider.GetComponentInParent<Rigidbody>();
             Debug.Assert(otherBody != null);
-            Vector3 relativePos = rootTransform.position - collider.transform.position;
+            Vector3 relativePos = collider.transform.position - rootTransform.position;
             Vector3 relativeVelocity = otherBody.linearVelocity - rigidBody.linearVelocity;
             float timeOfClosestApproach = Vector3.Dot(relativePos, relativeVelocity) / relativeVelocity.sqrMagnitude;
             // will there be a collision
@@ -36,24 +40,41 @@ public class CollisionAvoidanceBehavior : ISteeringBehavior
             }
 
             // also need to check against 0 here because that also means they don't collide
-            if (timeOfClosestApproach > 0 && shortestTimeToCollision > timeOfClosestApproach)
+            if (timeOfClosestApproach > 0 && bestTimeOfClosestApproach > timeOfClosestApproach)
             {
                 // this one is best so far
                 // save some data
+                // don't do unnecessary computations here because they might end up being useless
+                bestTarget = collider;
+                bestMinDistance = closestDistance;
+                bestCurrentDistance = (bestTarget.transform.position - rootTransform.position).magnitude;
+                bestCurrentRelativePosition = relativePos;
+                bestRelativePosition = 
+                bestRelativeVelocity = Vector3.zero;
+                bestTimeOfClosestApproach = 0;
             }
         }
 
-        var relativePos = 0;
+        Vector3 finalRelativePos = Vector3.zero;
 
         if (bestTarget == null)
         {
             return new SteeringOutput();
         }
-
-        if (bestMinDistance <= 0 || bestCurrentDistance <= 2 * collisionRadius)
+        // if we are already colliding or if we will hit exactly
+        if (bestMinDistance <= 0 || bestCurrentDistance.magnitude <= 2 * collisionRadius)
         {
-            relativePos = bestTarget.position - 
+            finalRelativePos = bestTarget.transform.position - rootTransform.position;
         }
+        else
+        {
+            finalRelativePos = bestRelativePosition + bestRelativeVelocity * bestTimeOfClosestApproach;
+        }
+
+        finalRelativePos.Normalize();
+        SteeringOutput output = new SteeringOutput();
+        output.linear = -1 * finalRelativePos * maxAcceleration;
+        return output;
     }
 
     public bool IsCompleted()
